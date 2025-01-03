e8a518bb01783f1bacac1835eb76058e37f72c78
Jace <jace@biyard.co>
2025-01-04 05:24:01 +0900

    featured 1. add test to survey

--- tests/constants.js
+++ tests/constants.js
@@ -1,11 +1,9 @@
 import path from "path";
 
 export const latency = Number(process.env.LATENCY) || 0;
-export const email = 'biyard.test.0@gmail.com';
-export const password = 'abcd1234!';
-export const agit_name = `Test Agit - ${process.env.SIGNATURE}`;
-export const collection_name = `Test Collection - ${process.env.SIGNATURE}`;
-export const nft_name = `Test NFT - ${process.env.SIGNATURE}`;
+export const longlatency = Number(process.env.LONGLATENCY) || 0;
+export const email = "biyard.test.0@gmail.com";
+export const password = "abcd1234!";
 export const image_path = path.join(__dirname, "../fixtures/images/");
 export const screenshot_path = (spec, scenario, name) =>
   path.join(__dirname, `../screenshots/${spec}-spec/${scenario}/${name}.png`);
